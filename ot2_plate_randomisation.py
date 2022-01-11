c247915d7e5cab66293b9a649a77678547ec0e26
Shaleen Sharma <shaleen@vowfood.com>
2022-01-11 15:16:19 +1100

    third commit

--- ot2_plate_randomisation.py
+++ ot2_plate_randomisation.py
@@ -111,5 +111,3 @@ def run(protocol: protocol_api.ProtocolContext):
            else: each_plate = Plate_2
            p1000_transfer_function(DSD_MEDIA_VOLUME, tube_rack_2, idx + 11, each_plate, each_well)
         p1000.drop_tip()
-
-        #hello
